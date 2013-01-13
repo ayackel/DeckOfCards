9935d1cc967094bf6ceae1f5873dc56eab51f48c
Alan Yackel <ayackel@gmail.com>
2013-01-13 00:42:58 -0500

    formatting

--- DeckOfCards/src/main/java/com/contentanalyst/carddeck/Card.java
+++ DeckOfCards/src/main/java/com/contentanalyst/carddeck/Card.java
@@ -6,52 +6,51 @@ public class Card {
 	private final Suit suit;
 
 	Card(Rank rank, Suit suit) {
-	  this.rank = rank;
-	  this.suit = suit;
+		this.rank = rank;
+		this.suit = suit;
 	}
 
-  public Rank getRank() {
-    return rank;
-  }
+	public Rank getRank() {
+		return rank;
+	}
 
-  public Suit getSuit() {
-    return suit;
-  }
+	public Suit getSuit() {
+		return suit;
+	}
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((rank == null) ? 0 : rank.hashCode());
-    result = prime * result + ((suit == null) ? 0 : suit.hashCode());
-    return result;
-  }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((rank == null) ? 0 : rank.hashCode());
+		result = prime * result + ((suit == null) ? 0 : suit.hashCode());
+		return result;
+	}
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    Card other = (Card) obj;
-    if (rank != other.rank) {
-      return false;
-    }
-    if (suit != other.suit) {
-      return false;
-    }
-    return true;
-  }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Card other = (Card) obj;
+		if (rank != other.rank) {
+			return false;
+		}
+		if (suit != other.suit) {
+			return false;
+		}
+		return true;
+	}
 
-  @Override
-  public String toString() {
-    return rank.getDisplay() + " of " + suit;
-  }
-
+	@Override
+	public String toString() {
+		return rank.getDisplay() + " of " + suit;
+	}
 
 }
--- DeckOfCards/src/main/java/com/contentanalyst/carddeck/Deck.java
+++ DeckOfCards/src/main/java/com/contentanalyst/carddeck/Deck.java
@@ -6,59 +6,58 @@ import java.util.EnumSet;
 import java.util.List;
 import java.util.Random;
 
-
 public class Deck {
 
 	private static final int CARDS_IN_FULL_DECK = 52;
-  private List<Card> cards = Collections.emptyList();
+	private List<Card> cards = Collections.emptyList();
 
-  private static final Random RNG = new Random();
+	private static final Random RNG = new Random();
 
-  public Deck() {
-    cards = createNewDeck();
-  }
+	public Deck() {
+		cards = createNewDeck();
+	}
 
-  List<Card> createNewDeck() {
-    List<Card> freshDeck = new ArrayList<Card>(CARDS_IN_FULL_DECK);
-    for(Rank rank : EnumSet.allOf(Rank.class)) {
-      for(Suit suit : EnumSet.allOf(Suit.class)) {
-        freshDeck.add(new Card(rank, suit));
-      }
-    }
-    return freshDeck;
-  }
+	List<Card> createNewDeck() {
+		List<Card> freshDeck = new ArrayList<Card>(CARDS_IN_FULL_DECK);
+		for (Rank rank : EnumSet.allOf(Rank.class)) {
+			for (Suit suit : EnumSet.allOf(Suit.class)) {
+				freshDeck.add(new Card(rank, suit));
+			}
+		}
+		return freshDeck;
+	}
 
-  public void shuffle() {
-    List<Card> freshDeck = createNewDeck();
-    cards = doShuffle(freshDeck);
-  }
+	public void shuffle() {
+		List<Card> freshDeck = createNewDeck();
+		cards = doShuffle(freshDeck);
+	}
 
 	/**
-	 * Fisher–Yates shuffle.  Randomizes the deck;
+	 * Fisher–Yates shuffle. Randomizes the deck;
 	 */
 	private List<Card> doShuffle(List<Card> freshDeck) {
-	  List<Card> shuffledDeck = new ArrayList<Card>();
-	  shuffledDeck.addAll(freshDeck);
-	  for(int i=1; i < CARDS_IN_FULL_DECK; i++) {
-	    int j = RNG.nextInt(i);
-	    shuffledDeck.set(i, shuffledDeck.get(j));
-	    shuffledDeck.set(j, freshDeck.get(i));
-	  }
+		List<Card> shuffledDeck = new ArrayList<Card>();
+		shuffledDeck.addAll(freshDeck);
+		for (int i = 1; i < CARDS_IN_FULL_DECK; i++) {
+			int j = RNG.nextInt(i);
+			shuffledDeck.set(i, shuffledDeck.get(j));
+			shuffledDeck.set(j, freshDeck.get(i));
+		}
 
-	  return shuffledDeck;
-  }
+		return shuffledDeck;
+	}
 
-  public Card dealsOneCard() {
+	public Card dealsOneCard() {
 		return cards.remove(cards.size() - 1);
 	}
 
-  int cardsInDeck() {
-    return cards.size();
-  }
+	int cardsInDeck() {
+		return cards.size();
+	}
 
-  void printDeck() {
-    for(Card card : cards) {
-      System.out.println(card);
-    }
-  }
+	void printDeck() {
+		for (Card card : cards) {
+			System.out.println(card);
+		}
+	}
 }
